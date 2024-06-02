dceda61b7c579e5f8411b378ba4ad983d7136b75
martindipeco <martindipeco@gmail.com>
2024-06-02 11:11:18 -0300

    validaciones

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -13,7 +14,7 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico)
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico)
     {
         System.out.println("Se recibió el pedido para registrar médico");
         System.out.println(datosRegistroMedico);
--- src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,5 +1,12 @@
 package med.voll.api.direccion;
 
-public record DatosDireccion(String calle, String distrito, String ciudad, String numero
-, String complemento) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosDireccion(
+        @NotBlank String calle,
+        @NotBlank String distrito,
+        @NotBlank String ciudad,
+        @NotBlank String numero,
+        String complemento) {
 }
