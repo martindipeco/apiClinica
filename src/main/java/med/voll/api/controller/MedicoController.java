ab8858177f87da617d24ddd6b9a2a02e1594452f
martindipeco <martindipeco@gmail.com>
2024-06-03 09:11:14 -0300

    Page y Pageable

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,7 +5,10 @@ import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,11 +28,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListadoMedico> listarMedicos()
+    public Page<DatosListadoMedico> listarMedicos(@PageableDefault (size = 2) Pageable paginacion)
     {
-        return medicoRepository.findAll()
-                .stream()
-                .map(DatosListadoMedico::new)
-                .toList();
+        return medicoRepository.findAll(paginacion)
+                .map(DatosListadoMedico::new);
     }
 }
