c283b510f11aefc58554eab616b8df68a1b56e6b
martindipeco <martindipeco@gmail.com>
2024-06-09 10:50:01 -0300

    controller de autenticacion

---
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.usuario.DatosAutenticacionUsuario;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacionController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario)
+    {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave()
+        );
+        authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}
