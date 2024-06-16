e2844cf940b65f763936a98f47e5ba1e249e7016
martindipeco <martindipeco@gmail.com>
2024-06-16 11:57:10 -0300

    query

--- src/main/java/med/voll/api/domain/consulta/AgendaDeconsultaService.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeconsultaService.java
@@ -4,6 +4,7 @@ import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.error.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +24,31 @@ public class AgendaDeconsultaService {
 
     public void agendar(DatosAgendarConsulta datosAgendarConsulta)
     {
-        var medicoOpcional = medicoRepository.findById(datosAgendarConsulta.idMedico());
-        Medico medico = null;
-        if (medicoOpcional.isPresent())
+        //validacion de id paciente
+        //TODO: no debería ser alrevés?? .isEmpty en vez de isPresent
+        //findByID devuelve un Optional
+        if(pacienteRepository.findById(datosAgendarConsulta.idPaciente()).isPresent())
         {
-            medico = medicoOpcional.get();
+            throw new ValidacionDeIntegridad("No se encontró ID de paciente");
         }
+
+        //validacion de medico.
+        //si no se envía medico, elegir de acuerdo a disponibilidad y especialidad
+        //existById devuelve un boolean
+        //si no es null y existe, ¿se lanza la excepción???? (TODO: no debería ser alreves??)
+        if(datosAgendarConsulta.idMedico()!=null && medicoRepository.existsById(datosAgendarConsulta.idMedico()))
+        {
+            throw new ValidacionDeIntegridad("No se encontró ID de médico");
+        }
+
+
+//        var medicoOpcional = medicoRepository.findById(datosAgendarConsulta.idMedico());
+//        Medico medico = null;
+//        if (medicoOpcional.isPresent())
+//        {
+//            medico = medicoOpcional.get();
+//        }
+        var medico = seleccionarMedico(datosAgendarConsulta);
 
         var pacienteOpcional = pacienteRepository.findById(datosAgendarConsulta.idPaciente());
         Paciente paciente = null;
@@ -39,5 +59,23 @@ public class AgendaDeconsultaService {
 
         var consulta = new Consulta(null, medico, paciente, datosAgendarConsulta.fecha());
         consultaRepository.save(consulta);
+    }
+
+    private Medico seleccionarMedico(DatosAgendarConsulta datosAgendarConsulta) {
+        //si el id no es null, devolvemos el medico por id
+        if(datosAgendarConsulta.idMedico()!= null)
+        {
+            return medicoRepository.getReferenceById(datosAgendarConsulta.idMedico());
+        }
+
+        //si no vino especialidad, devolvemos error
+        if(datosAgendarConsulta.especialidad()==null)
+        {
+            throw new ValidacionDeIntegridad("debe seleccionar una especialidad");
+        }
+
+        //si no vino dato de medico, pero sí tenemos dato de especialidad, elegimos aleatorio por fecha y hora
+        return medicoRepository.seleccionarMedicoConEspecialidadEnFecha(datosAgendarConsulta.especialidad(),
+                datosAgendarConsulta.fecha());
     }
 }
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -3,9 +3,24 @@ package med.voll.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
+
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE m.activo = 1 AND m.especialidad = :especialidad AND
+            m.id NOT IN (
+            SELECT c.medico.id FROM Consulta c
+            c.fecha = .fecha
+            )
+            ORDER BY rand()
+            LIMIT 1
+            """)
+    Medico seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 
     //Page<Medico> findAll(Pageable pageable);
 }
